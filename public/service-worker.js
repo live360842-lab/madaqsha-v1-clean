b912e1c99ac32f772e5348e9a82c7528b4cc0c81
live360842-lab <live360842@gmail.com>
2026-06-14 20:08:43 +0300

    Add non-aggressive service worker

---
+++ public/service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'madaqsha-v1-dev-20260614';
+const ASSETS = ['/', '/index.html', '/styles.css', '/client.js', '/manifest.webmanifest'];
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then((res) => res || caches.match('/index.html'))));
+});
